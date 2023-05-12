a21e1a6aadf67db4fb450d00caec2a7b54505536
exec zsh <candice.fays@ymail.com>
2023-05-13 01:10:36 +0200

    routes ok, need seeds pour show

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,5 +1,12 @@
 class ChallengesController < ApplicationController
+  def home
+  end
+
   def index
     @challenges = Challenge.all
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
   end
 end
